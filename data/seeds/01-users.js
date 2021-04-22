21278c4132a24e74c17f4dc5f1c9801315a95a30
Nico Herrera <nherrera0831@gmail,com>
2021-04-21 21:13:59 -0700

    added cleanup seeds

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,13 +1,8 @@
 
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
-      // Inserts seed entries
       return knex('users').insert([
         {id: 1, username: 'Nico', password: "1234", phone_number: "999-999-9999"},
         {id: 2, username: 'Kyle', password: "1234", phone_number: "999-999-9999"},
         {id: 3, username: 'Sam', password: "1234", phone_number: "999-999-9999"}
       ]);
-    });
 };
